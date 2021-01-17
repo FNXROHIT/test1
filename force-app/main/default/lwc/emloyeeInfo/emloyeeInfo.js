4467a5d6cc10e3103b63b471b8add3e1eedbe4f9
Rohit <rohitsinghal1997@gmail.com>
2021-01-17 11:22:23 +0530

    init

---
+++ force-app/main/default/lwc/emloyeeInfo/emloyeeInfo.js
@@ -0,0 +1,32 @@
+import { LightningElement, track } from 'lwc';
+
+export default class EmloyeeInfo extends LightningElement {
+    @track name;
+    @track age;
+    @track salary;
+    @track show=true;
+
+
+    nameHandler(event)
+    {
+        this.name = event.target.value;
+
+    }
+
+    ageHandler(event)
+    {
+        this.age = event.target.value;
+        
+    }
+
+    salaryHandler(event)
+    {
+        this.salary = event.target.value;
+        
+    }
+
+    saveButtonHandler(){
+        this.show=false;
+    }
+
+}
---
+++ force-app/main/default/lwc/employeeDetailExample/employeeDetailExample.js
@@ -0,0 +1,19 @@
+import { LightningElementsd,track } from 'lwc';
+
+export default class EmployeeDetailExample extends LightningElement {
+    @track name;
+    @track age;
+    @track salary;
+
+    nameHandler(event){
+        this.name = event.target.value;
+    }
+
+    ageHandler(event){
+        this.name = event.target.value;
+    }
+
+    salaryHandler(event){
+        this.name = event.target.value;
+    }
+}
